3248c74f477087ff7cc4b017760fb52f8194b129
Marco De Canal <marco.decanal@gmail.com>
2023-01-05 23:40:22 +0100

    store

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -1,14 +1,39 @@
 export const print1 = (storeAPI) => (next) => (action) => {
-  console.log('1')
-  return next(action)
-}
+  console.log('1');
+  return next(action);
+};
 
 export const print2 = (storeAPI) => (next) => (action) => {
-  console.log('2')
-  return next(action)
-}
+  console.log('2');
+  return next(action);
+};
 
 export const print3 = (storeAPI) => (next) => (action) => {
-  console.log('3')
-  return next(action)
+  console.log('3');
+  return next(action);
+};
+
+export function loggerMiddleware(storeAPI) {
+  return function wrapDispatch(next) {
+    return function handleAction(action) {
+      //Before executing reducer...
+      console.log('Dispatching', action);
+
+      //Entering middlewares' pipeline...
+      let result = next(action);
+
+      //After reducer execution...
+      console.log('next state', storeAPI.getState());
+      return result;
+    };
+  };
 }
+
+export const delayedMessageMiddleware = (storeAPI) => (next) => (action) => {
+  if (action.type === 'todos/todoAdded') {
+    setTimeout(() => {
+      console.log('Added a new todo 1 sec ago: ', action.payload);
+    }, 1000);
+  }
+  return next(action);
+};
--- src/store.js
+++ src/store.js
@@ -1,5 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducer';
+import {
+  print1,
+  print2,
+  print3,
+  loggerMiddleware,
+  delayedMessageMiddleware,
+} from './exampleAddons/middleware';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import {
+//   sayHiOnDispatch,
+//   includeMeaningOfLife,
+// } from './exampleAddons/enhancers';
 
 let preloadedState;
 const persistedTodosString = localStorage.getItem('todos');
@@ -9,6 +21,22 @@ if (persistedTodosString) {
     todos: JSON.parse(persistedTodosString),
   };
 }
-const store = createStore(rootReducer, preloadedState);
 
+// const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife);
+
+// const store = createStore(rootReducer, preloadedState, composedEnhancer);
+
+// const middlewareEnhancer = applyMiddleware(
+//   loggerMiddleware,
+//   delayedMessageMiddleware,
+//   print1,
+//   print2,
+//   print3
+// );
+// const store = createStore(rootReducer, middlewareEnhancer);
+
+const composedEnhancer = composeWithDevTools(
+  applyMiddleware(delayedMessageMiddleware)
+);
+const store = createStore(rootReducer, composedEnhancer);
 export default store;
